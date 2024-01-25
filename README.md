b6ef9c0eb289e49983fcf7f2f70b0518ae64efaa
Max Perry <me@maxperry.dev>
2024-01-25 12:29:03 +0000

    feat: update install script to backup existing dotfiles and create symlinks for the ones in this repo

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Dotfiles
 
-My dotfiles and system set up for MacOS.
+This contains my dotfiles and some system setup utilities for MacOS.
+
+## Install
+
+To install the dotfiles, run the below command:
+
+```bash
+ curl -fsSL https://raw.githubusercontent.com/maximus1108/dotfiles/master/install.sh | bash
+```
+
+This will install git on the system via Homebrew if it doesn't already exist.
--- install.sh
+++ install.sh
@@ -1,11 +1,42 @@
 #!/usr/bin/env bash
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if ! command -v git &> /dev/null; then
+  if ! command -v brew &> /dev/null; then
+    echo "Installing Homebrew."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  echo "Installing git."
+  brew install git
+fi
 
-# Install git
-brew install git
+dotfiles_dir=$HOME/.dotfiles
 
-# Clone this repo
-git clone https://github.com/maximus1108/dotfiles.git ~/.dotfiles
+if [ -d $dotfiles_dir ]; then
+  while read -p  "Dotfiles directory '$dotfiles_dir' already exists, would you like to overwrite it? (yes/no) " yn; do
+    case $yn in
+      yes) echo "Deleting $dotfiles_dir"; rm -rf $dotfiles_dir; break;;
+      no) exit;;
+      * ) echo "Please answer yes or no.";;
+    esac
+  done
+fi
 
+echo "Creating dotfiles directory '$dotfiles_dir'."
+mkdir -p $dotfiles_dir
+
+echo "Cloning dotfiles repository into '$dotfiles_dir'."
+git clone --quiet https://github.com/maximus1108/dotfiles.git $dotfiles_dir
+cd $dotfiles_dir
+
+dotfiles=$(ls -a dotfiles | xargs -I{} bash -c "if ! [ -d {} ]; then echo {}; fi")
+
+backup_folder=$HOME/.dotfiles-backup
+mkdir -p $backup_folder
+for dotfile in $dotfiles; do
+  if [ -f $HOME/$dotfile ]; then
+    echo "Backing up pre-existing $dotfile in $backup_folder.";
+    mv $HOME/$dotfile $backup_folder/$dotfile
+  fi
+  echo "Creating symlink for $dotfile."
+  ln -s -f $dotfiles_dir/dotfiles/$dotfile $HOME/$dotfile
+done
